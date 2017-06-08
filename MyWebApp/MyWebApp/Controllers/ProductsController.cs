6fb6bad82afd2412b8737a40351a39fab3235bb7
dixanta <dixanta@gmail.com>
2017-06-08 19:32:52 +0545

    first commit

---
+++ MyWebApp/MyWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MyWebApp.Models;
+
+namespace MyWebApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private List<Product> products;
+
+        public ProductsController()
+        {
+            products= new List<Product>()
+            {
+                new Product()
+                {
+                    Id=1,
+                    Name="Samsung",
+                    Category="Mobile",
+                    Price=25000
+                },
+                new Product()
+                {
+                    Id=2,
+                    Name="IPhone",
+                    Category="Mobile",
+                    Price=25000
+                }
+            };
+        }
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = from p in products where p.Id == id select p;
+            return Ok(product.FirstOrDefault());
+        }
+
+        public IHttpActionResult Post(Product product)
+        {
+            return Ok(product);
+        }
+
+       
+    }
+}
